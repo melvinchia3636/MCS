4576517b8aa04a247c5f6c22839018712c912522
melvinchia3636 <melvinchia623600@gmail.com>
2022-01-15 06:47:38 +0800

    enter

--- client/src/components/search/SearchInput.jsx
+++ client/src/components/search/SearchInput.jsx
@@ -13,11 +13,17 @@ function SearchInput() {
     navigate(url);
   };
 
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      gotoSearchResult();
+    }
+  };
+
   return (
-    <div className="p-20">
+    <div className="p-20 w-full">
       {mode === null ? (
         <>
-          <h1 className="font-medium text-5xl text-amber-400">Let&apos;s find you a server!</h1>
+          <h1 className="font-medium text-5xl text-amber-400 mb-3">Let&apos;s find you a server!</h1>
           <p className="text-xl tracking-wide mb-8 text-slate-600 dark:text-white duration-500 transition-all">What would you like to do? :)</p>
           <div className="flex w-full items-center justify-center gap-4">
             <button onClick={() => setMode(0)} type="button" className="w-full h-16 flex items-center justify-center gap-2 bg-slate-100 dark:bg-zinc-600 shadow-md font-medium text-xl text-slate-700 dark:text-white rounded-md transition-all duration-500">
@@ -47,7 +53,7 @@ function SearchInput() {
           </p>
           <div className="shadow-md p-6 rounded-md text-xl overflow-hidden flex items-center justify-center w-full gap-2 bg-white dark:bg-zinc-600 transition-all duration-500">
             <Icon icon="uil:search" width="28" height="28" className="text-slate-400" />
-            <input onChange={(e) => setQuery(e.target.value)} onEnter={gotoSearchResult} type="text" placeholder="Type your keyword here..." className="w-full tracking-wide text-slate-700 dark:text-white duration-500 transition-all focus:outline-none bg-transparent" />
+            <input onChange={(e) => setQuery(e.target.value)} onEnter={gotoSearchResult} onKeyDown={handleKeyDown} type="text" placeholder="Type your keyword here..." className="w-full tracking-wide text-slate-700 dark:text-white duration-500 transition-all focus:outline-none bg-transparent" />
           </div>
           <button onClick={gotoSearchResult} type="button" className="w-full h-16 bg-amber-400 flex items-center justify-center gap-1 font-medium text-2xl text-white shadow-md rounded-md">
             Go!
